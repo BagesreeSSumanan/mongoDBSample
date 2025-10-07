88e209c96237ea46075ac2b64e0c04653ac2058c
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-10-07 13:30:05 +0530

    code optimization

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,20 +1,20 @@
 const Contact = require('../models/Contact');
 
-const createContact = async (req,res) =>{
-     try{
-            const newContact = new Contact(req.body);
-            await newContact.save()
-            .then((savedContact =>{
-                console.log(savedContact);
-                res.status(201).json({msg:"Conatct saved Successfully"})
-            }))
-            .catch((error)=>{
-                console.log(error);
-                res.status(500).json({msg:"Unable to create new contact"})
-            })
-        }catch(error){
-            console.log(error);
-            res.status(500).json({msg:"Unable to create new contact"})
-        }
-}
+const createContact = async (req, res) => {
+    try {
+    if (!req.body) {
+      return res.sendStatus(400);
+    }
+ 
+    const newContact = new Contact(req.body);
+    const savedContact = await newContact.save();
+ 
+    console.log(savedContact);
+    return res.status(201).json({ msg: "Contact saved successfully", data: savedContact });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Unable to create new contact", error: error.message });
+  }
+};
+
 module.exports = {createContact}
--- src/router/contactRouter.js
+++ src/router/contactRouter.js
@@ -2,17 +2,5 @@ const express = require('express');
 const router = express.Router();
 const {createContact}= require('../controller/controller');
 
-router.post("/Contact",async(req,res)=>{
-    try{
-        
-    if (!req.body) {
-        return res.sendStatus(400);
-    }
-        const newContact=  await createContact(req,res).then(() => res.json({ message: 'New Contact created.' }));
-
-    } catch(e){
-        console.log(e);
-        res.sendStatus(400);
-    }
-})
+router.post("/Contact", createContact);
 module.exports = router;
