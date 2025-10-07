82ad436b0c558489dc8c9d65d5eaf1852f53e4de
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-10-07 14:12:55 +0530

    update , delete and get

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -17,4 +17,87 @@ const createContact = async (req, res) => {
   }
 };
 
-module.exports = {createContact}
+const UpdateContact = async (req, res) => {
+    try {
+    const { id } = req.params; 
+    if (!id|| !req.body) {
+      return res.sendStatus(400);
+    }
+ 
+    const updatedContact = await Contact.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true } 
+    );
+
+    if (!updatedContact) {
+      return res.status(404).json({ msg: "Contact not found" });
+    };
+ 
+    console.log(updatedContact);
+    return res.status(201).json({ msg: "Contact updated successfully", data: updatedContact });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Unable to update the contact", error: error.message });
+  }
+};
+const GetContact = async (req, res) => {
+    try {
+    const { id } = req.params; 
+    if (!id) {
+      return res.sendStatus(400);
+    }
+ 
+    const CurrentContact = await Contact.findById(
+      id,
+     'fullName emailAddress age'
+    );
+
+    if (!CurrentContact) {
+      return res.status(404).json({ msg: "Contact not found" });
+    };
+ 
+    console.log(CurrentContact);
+    return res.status(201).json({ msg: "Contact details", data: CurrentContact });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Unable to get the contact", error: error.message });
+  }
+};
+const GetAllContact = async (req, res) => {
+    try {
+ 
+     const Contacts = await Contact.find({}, 'fullName emailAddress age');
+
+    if (!Contacts) {
+      return res.status(404).json({ msg: "Contact not found" });
+    };
+ 
+    console.log(Contacts);
+    return res.status(201).json({ msg: "Contact details", data: Contacts });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Unable to get the contact", error: error.message });
+  }
+};
+const DeleteContact = async (req, res) => {
+    try {
+    const { id } = req.params; 
+    if (!id) {
+      return res.sendStatus(400);
+    }
+ 
+    const result = await Contact.deleteOne({ _id: id });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ msg: "Contact not found" });
+    }
+    else{
+        return res.status(201).json({ msg: "Deleted succesfully" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Unable to delete the contact", error: error.message });
+  }
+};
+module.exports = {createContact,UpdateContact,GetContact, GetAllContact,DeleteContact}
--- src/router/contactRouter.js
+++ src/router/contactRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {createContact}= require('../controller/controller');
+const {createContact,UpdateContact,GetContact,GetAllContact,DeleteContact}= require('../controller/controller');
 
 router.post("/Contact", createContact);
+router.put("/UpdateContact/:id", UpdateContact);
+router.get("/GetContact/:id", GetContact);
+router.get("/GetAllContact", GetAllContact);
+router.delete("/DeleteContact/:id", DeleteContact);
 module.exports = router;
